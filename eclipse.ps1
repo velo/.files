71ba16cf5428694df7ec10b5f36efe2ed69f0bcb
Marvin Froeder <marvin.froeder@vizexplorer.com>
2016-01-27 21:10:41 +1300

    Better support for 4k screens

--- eclipse.ps1
+++ eclipse.ps1
@@ -1,6 +1,6 @@
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($env:USERPROFILE + "\Start Menu" + "\Eclipse.lnk")
 $Shortcut.TargetPath = "c:\tools\eclipse\VizIDE.exe"
-$Shortcut.Arguments = "-vm `"c:\Program Files\Java\jdk1.8.0_72\bin\javaw.exe`""
+$Shortcut.Arguments = "-vm `"C:\Program Files\Java\jdk1.8.0_72\jre\bin\server\jvm.dll`""
 $Shortcut.WorkingDirectory = "c:\tools\eclipse\"
 $Shortcut.Save()
