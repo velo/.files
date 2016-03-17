64932f580a33d7342a8a7ca7479145bc661bd5e8
Marvin Froeder <marvin.froeder@vizexplorer.com>
2016-03-17 15:04:51 +1300

    Remove Git mergetool unwanted .orig files

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,6 +27,7 @@ echo '	tool = bc3'
 
 git config --global merge.tool bc3
 git config --global mergetool.bc3.path "c:/Program Files/Beyond Compare 4/bcomp.exe"
+git config --global mergetool.keepBackup false
 
 docker-machine create -d "virtualbox" dm
 eval "$(docker-machine env dm)"
